3724294e46502a7a7e3345f3202ed3e19fefccca
usman-ali-2000 <122299296+usman-ali-2000@users.noreply.github.com>
2024-03-20 12:44:06 +0500

    Update FinancialSeason.js

--- components/FinancialSeason.js
+++ components/FinancialSeason.js
@@ -1,1 +1,17 @@
 
+const mongoose = require("mongoose");
+
+const financialseasonSchema = new mongoose.Schema({
+    email:{
+      type: String,
+    },
+    farm:{
+        type: String,
+    },
+    date:{
+      type: String,
+    }
+  });
+
+const uploadFinancialSeason = new mongoose.model('uploadFinancialSeason', financialseasonSchema);
+module.exports = uploadFinancialSeason;
